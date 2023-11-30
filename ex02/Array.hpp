d6938faf1430c1eb2b435c038d904934c5b633b4
fheld <fheld@student.42berlin.de>
2023-11-30 21:19:03 +0100

    sceleton functions with logic and functionality missing but types (mostly) there

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -4,15 +4,58 @@ template<typename t>
 class Array
 {
 public:
-	// void foo(t a)
-	// {
-	// 	std::cout << a ;
-	// }
-	void foo(t a);
+	Array();
+	Array(unsigned int n);
+	Array(Array<t>& a);
+	Array<t>& operator=(Array<t> rhs);
+	~Array();
+	t operator[](unsigned int n);
+
+	unsigned int size();
 };
 
-template<typename t>
-void Array<t>::foo(t a)
+template<typename t> 
+Array<t>::Array()
 {
-	std::cout << a ;
+	std::cout << "im an array" << std::endl;
 }
+
+template<typename t> 
+Array<t>::Array(unsigned int n)
+{
+	std::cout << "im n'n unsigned int n array" << n << std::endl;
+}
+
+template<typename t> 
+Array<t>::Array(Array<t>& a)
+{
+	(void) a;
+	std::cout << "im a template array" << std::endl;
+}
+
+template<typename t> 
+Array<t>::~Array()
+{
+	std::cout << "im a wiggle array" << std::endl;
+}
+
+template<typename t> 
+unsigned int Array<t>::size()
+{
+	return 11;
+}
+
+template<typename t> 
+Array<t>& Array<t>::operator=(Array<t> rhs)
+{
+	(void) rhs;
+	return *this;
+}
+
+template<typename t> 
+t Array<t>::operator[](unsigned int n)
+{
+	(void) n;
+	return 70;
+}
+
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,13 +3,19 @@
 
 int main()
 {
-	// class Array<int> myarr;
-	// myarr.foo(1342);
-	Array<int> myarr;
-	myarr.foo(1342);
+	Array<int> myarr1;
+	Array<int> myarr2(myarr1);
+	Array<int> myarr3(123);
 
-	Array<std::string> s;
-	s.foo("all fish are green");
+	std::cout << myarr1.size() << std::endl;
+	std::cout << myarr1[3] << std::endl;
 
+	//char
+	Array<char> myarr4(123);
+	std::cout << myarr4[3] << std::endl;
+
+	(void) myarr1;
+	(void) myarr2;
+	(void) myarr3;
 	return 0;
 }
