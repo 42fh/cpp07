cc3a810912bdd1b5d3b4ed8669047a59cfe9e668
fheld <fheld@student.42berlin.de>
2023-12-03 15:54:49 +0100

    added functionality of array class

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,61 +1,89 @@
+#pragma once
+
 #include <iostream>
 
 template<typename t>
 class Array
 {
+private:
+	unsigned int numberOfElements;
+	t* arr;
 public:
 	Array();
 	Array(unsigned int n);
 	Array(Array<t>& a);
 	Array<t>& operator=(Array<t> rhs);
 	~Array();
-	t operator[](unsigned int n);
+	t& operator[](unsigned int n);
+	t operator[](unsigned int n) const;
 
 	unsigned int size();
+	class OutOfBound: public std::exception{};
 };
 
 template<typename t> 
-Array<t>::Array()
+Array<t>::Array(): numberOfElements(0), arr(NULL)
 {
-	std::cout << "im an array" << std::endl;
 }
 
 template<typename t> 
-Array<t>::Array(unsigned int n)
-{
-	std::cout << "im n'n unsigned int n array" << n << std::endl;
+Array<t>::Array(unsigned int n): numberOfElements(n)
+{	
+	arr = new t[n]();
 }
 
 template<typename t> 
-Array<t>::Array(Array<t>& a)
+Array<t>::Array(Array<t>& other): numberOfElements(other.numberOfElements)
 {
-	(void) a;
-	std::cout << "im a template array" << std::endl;
+	this->arr = new t[numberOfElements]();
+	for (unsigned int i = 0; i < numberOfElements; i++)
+	{
+		this->arr[i] = other.arr[i];
+	}
 }
 
 template<typename t> 
 Array<t>::~Array()
 {
-	std::cout << "im a wiggle array" << std::endl;
+	delete[] this->arr;
 }
 
 template<typename t> 
 unsigned int Array<t>::size()
 {
-	return 11;
+	return numberOfElements;
 }
 
 template<typename t> 
 Array<t>& Array<t>::operator=(Array<t> rhs)
 {
-	(void) rhs;
+	delete[] this->arr;
+
+	this->numberOfElements = rhs.numberOfElements;
+	this->arr = new t[this->numberOfElements]();
+	for (unsigned int i = 0; i < numberOfElements; i++)
+	{
+		this->arr[i] = rhs.arr[i];
+	}
 	return *this;
 }
 
 template<typename t> 
-t Array<t>::operator[](unsigned int n)
+t& Array<t>::operator[](unsigned int n)
 {
-	(void) n;
-	return 70;
+	if (n >= numberOfElements || numberOfElements == 0)
+	{
+		throw OutOfBound();
+	}
+	return arr[n];
 }
 
+template<typename t> 
+t Array<t>::operator[](unsigned int n) const
+{
+	if (n >= numberOfElements)
+	{
+		throw OutOfBound();
+	}
+	return arr[n];
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,21 +1,69 @@
 #include <iostream>
 #include "Array.hpp"
 
+
+template<typename ty>
+void test()
+{
+	std::cout << "Construction with no parameter" << std::endl;
+	Array<ty> myarr1;
+	std::cout << "Copy construction" << std::endl;
+	Array<ty> myarr2(myarr1);
+	Array<ty> myarr3(3);
+	Array<ty> myarr4(myarr3);
+
+
+	std::cout << "Size of default construtor" << std::endl;
+	std::cout << myarr1.size() << std::endl;
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		myarr3[i] = 70 + i;
+	}
+	
+	std::cout << "trying to access invalid pos 2" << std::endl;
+	try
+	{
+		std::cout << myarr1[2] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "trying to access invalid pos -1 " << std::endl;
+	try
+	{
+		std::cout << myarr1[-1] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "copy assignment operator" << std::endl;
+	myarr1 = myarr3;
+
+	std::cout << "accessing valid position" << std::endl;
+	try
+	{
+		ty n = myarr1[2];
+		std::cout << "myarr1[2] is " << n << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+
 int main()
 {
-	Array<int> myarr1;
-	Array<int> myarr2(myarr1);
-	Array<int> myarr3(123);
+	std::cout << "========== testing for int" << std::endl;
+	test<int>();
+	std::cout << "========== testing for unsigned int" << std::endl;
+	test<unsigned int>();
+	std::cout << "========== testing for char" << std::endl;
+	test<char>();
 
-	std::cout << myarr1.size() << std::endl;
-	std::cout << myarr1[3] << std::endl;
-
-	//char
-	Array<char> myarr4(123);
-	std::cout << myarr4[3] << std::endl;
-
-	(void) myarr1;
-	(void) myarr2;
-	(void) myarr3;
 	return 0;
 }
