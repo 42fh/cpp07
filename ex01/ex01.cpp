5df3e925f762d7cf42308cef5dda15a28cb69129
fheld <fheld@student.42berlin.de>
2024-05-02 18:42:16 +0200

    added overloads of iteration function

--- ex01/ex01.cpp
+++ ex01/ex01.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include "iter.hpp"
+
 class Awesome
 {
   public:
--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,6 +1,48 @@
-template <typename tisch>
-void iter(tisch *arr, size_t n, void (*foo)(tisch))
+template <typename T>
+void iter(T *arr, const size_t n, void (*foo)(T))
 {
+	if (arr == NULL || foo == NULL)
+		throw(std::exception());
+
+	for (size_t i = 0; i < n; i++)
+	{
+		foo(*arr);
+		arr++;
+	}
+}
+
+template <typename T>
+void iter(const T *arr, const size_t n, void (*foo)(const T))
+{
+	if (arr == NULL || foo == NULL)
+		throw(std::exception());
+
+	for (size_t i = 0; i < n; i++)
+	{
+		foo(*arr);
+		arr++;
+	}
+}
+
+template <typename T>
+void iter(T *arr, const size_t n, void (*foo)(T&))
+{
+	if (arr == NULL || foo == NULL)
+		throw(std::exception());
+
+	for (size_t i = 0; i < n; i++)
+	{
+		foo(*arr);
+		arr++;
+	}
+}
+
+template <typename T>
+void iter(const T *arr, const size_t n, void (*foo)(const T&))
+{
+	if (arr == NULL || foo == NULL)
+		throw(std::exception());
+
 	for (size_t i = 0; i < n; i++)
 	{
 		foo(*arr);
